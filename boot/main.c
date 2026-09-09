073910f7c64d71ff1af68a7d72d89d94927a3d1a
wija <wijaking@outlook.com>
2026-09-09 16:59:15 +0100

    added old files back

---
+++ boot/main.c
@@ -0,0 +1,10 @@
+#include <uefi.h>
+#include <stdbool.h>
+
+int main() {
+    printf("fos.\n");
+    printf("Launched fos bootloader.\n");
+    printf("Version 0.0.1");
+    while (true);
+    return 0;
+}
